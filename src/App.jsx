edacdca9d16804081493f9a0fc2648af2aa82627
ghost91202 <karansharma8543@gmail.com>
2024-01-21 15:04:35 +0530

    added about pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './conponents/layout/home.jsx'; // Adjust the import path if needed
+
+import About from "./conponents/layout/Aboutpage.jsx"
+import Team from "./conponents/layout/Team.jsx"
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <Router>
+            <Routes className="md:overflow-hidden overflow-hidden w-full">
+                <Route path="/" element={<Home />} />
 
-export default App
+                <Route path="/About" element={<About/>} />
+                <Route path="/Team" element={<Team/>} />
+
+
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
