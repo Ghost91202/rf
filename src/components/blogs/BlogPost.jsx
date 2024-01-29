d8f25b7341b6bc575a3152ba110d0f208a33cdb5
Ghost91202 <karansharma8543@gmail.com>
2024-01-29 15:17:27 +0500

    olx blog post

--- src/components/blogs/BlogPost.jsx
+++ src/components/blogs/BlogPost.jsx
@@ -1,7 +1,7 @@
 // components/Blog/BlogPost.js
 import React from 'react';
 import BlogVideo from './BlogVideo.jsx';
-const BlogPost = ({ post }) => {
+const BlogPost = ( { post } ) => {
   return (
     <div className="max-w-2xl mx-auto mt-8 p-10 rounded-md shadow-md">
       <h1 className="text-4xl text-center border-b-2 border-orange-400 font-semibold mb-6">{post.title}</h1>
@@ -12,15 +12,33 @@ const BlogPost = ({ post }) => {
       <p className="text-gray-700 mb-4">{post.para1}</p>
       <p className="text-gray-700 mb-4">{post.para2}</p>
       <p className="text-gray-700 mb-4">{post.para3}</p>
-
       {post.imgurl && <img src={post.imgurl} alt="rahil foundation" className="w-full mb-6 rounded-md" />}
-
       <h2 className="text-2xl font-semibold mb-4">{post.Heading2}</h2>
       <p className="text-gray-700 mb-4">{post.para4}</p>
 
       <h2 className="text-2xl font-semibold mb-4">{post.Heading3}</h2>
+      <h2 className="text-gray-700  mb-4">{post.content1}</h2>
+
+      <h2 className="text-2xl font-semibold mb-4">{post.Heading4}</h2>
       <p className="text-gray-700 mb-4">{post.para5}</p>
 
+      <h2 className="text-2xl font-semibold mb-4">{post.Heading5}</h2>
+      <p className="text-black-700 font-semibold mb-4">{post.list1}</p>
+      <p className="text-gray-700 mb-4">{post.content2}</p>
+
+      <p className="text-black-700 font-semibold mb-4">{post.list2}</p>
+      <p className="text-gray-700 mb-4">{post.content3}</p>
+
+      <p className="text-black-700 font-semibold mb-4">{post.list3}</p>
+      <p className="text-gray-700 mb-4">{post.content4}</p>
+
+      <p className="text-black-700 font-semibold mb-4">{post.list4}</p>
+      <p className="text-gray-700 mb-4">{post.content5}</p>
+
+      <p className="text-black-700 font-semibold mb-4">{post.list5}</p>
+      <p className="text-gray-700 mb-4">{post.content6}</p>
+
+     
       <BlogVideo videoUrl={post.videoUrl} />
 
 
--- src/components/blogs/data/blogData.jsx
+++ src/components/blogs/data/blogData.jsx
@@ -2,7 +2,7 @@
 const blogData = [
   {
     id: 1,
-    coverImageUrl:"https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
+    coverImageUrl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
     title: "PHISHING AWARENESS",
     para1: `Rahil Foundation is at the Frontline of Promoting Cyber Literacy,
     with a Particular Focus on raising Awareness about Phishing – a
@@ -34,11 +34,11 @@ const blogData = [
     content: `In an increasingly interconnected world, where our personal and professional lives revolve around digital devices, the importance of safeguarding our data cannot be overstated. Rahil Foundation is committed to promoting digital security literacy, and in this comprehensive guide, we will explore essential practices to keep your data secure on both computers and mobile devices. If you are interested in how to keep your data secure, kindly read our page Understanding the Importance of Data Security.`,
     imgurl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
     videoUrl: "https://www.youtube.com/embed/your_video_id", // Replace with your actual video URL
-    relatedBlogs: [2, 3],
+    relatedBlogs: [ 2, 3 ],
   },
   {
     id: 2,
-    coverImageUrl:"https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
+    coverImageUrl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
 
     title: "Social Media Safety",
     para1: `As Our Lives Increasingly Intertwine with the Digital Nation, Rahil
@@ -75,44 +75,97 @@ conversations.`,
     content: `In an increasingly interconnected world, where our personal and professional lives revolve around digital devices, the importance of safeguarding our data cannot be overstated.Rahil Foundation is committed to promoting digital security literacy, and in this comprehensive guide, we will explore essential practices to keep your data secure on both computers and mobile devices.If you are interested in how to keep your data secure, kindly read our page Understanding the Importance of Data Security.`,
     imgurl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
     videoUrl: "https://www.youtube.com/embed/your_video_id", // Replace with your actual video URL
-    relatedBlogs: [1, 3],
+    relatedBlogs: [ 1, 3 ],
   },
   {
     id: 3,
-    coverImageUrl:"https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
+    coverImageUrl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
 
-    title: "PHISHING AWARENESS",
-    para1: `Rahil Foundation is at the Frontline of Promoting Cyber Literacy,
-    with a Particular Focus on raising Awareness about Phishing – a
-    Common and Misleading form of Cybercrime.`,
-    para2: `Phishing is a Method Cybercriminals use to Trick Individuals into
-    Leak Sensitive Information such as Usernames, Passwords, and
-    Financial Details.It often involves Fraudulent Emails, Messages, or
-    Websites that Appear Trustworthy but Aim to Benefit from
-    Unsuspecting Victims.`,
+    title: "OLX USERS FRAUD",
+    Heading1: `Harshita Kejriwal OLX User Scam from UPI `,
+    para1: ` Today's Era is the Era of Artificial Intelligence. The threats of Cyber
+Scams are Increasing rapidly Every Year. Harshita Kejriwal (Daughter
+of Delhi Scam Arvind Kejriwal) has also become a Victim of this Scam.
+Scammer has defrauded them of Rs. 34,000. OLX, The Popular Online
+Marketplace, has Revolutionized the Way People Buy and Sell used
+items. With Its User-Friendly Interface and Wide Range of Products,
+OLX has become a go-to Platform for many.
 
-    para3: `Rahil Foundation Recognizes the Urgent need for Comprehensive
-    cyber literacy, especially in the realm of Phishing Awareness.In this
-    content, We delve into the Key Aspects of Phishing, It’s Various
-    Forms and Practical tips to Stay Vigilant in the Digital Landscape.
+`,
+    para2: `However, Amidst the Convenience and Ease, there is a Growing
+Concern of Frauds on OLX. Scammers, taking Advantage of
+Unsuspecting users, Employ various tactics to deceive and scam
+them.`,
+
+    para3: `In this Blog, we will Delve into the world of OLX Frauds, understand
+How they Operate and most Importantly, learn how to Protect
+Ourselves and file a Complaint if We fall Victim to such Frauds.
     `,
-    Heading1: `Understanding Phishing: `,
-    para4: `Phishing attacks can take Different forms but They Generally share
-    the Common Goal of Deceiving Individuals.Cybercriminals often
-    Employ Social Engineering Techniques to Manipulate Emotions and
-    Prompt hasty actions.It could be an urgent email claiming to be
-    from a reputable institution, a fake website mimicking a familiar
-    platform, or even messages appearing to be from friends or
-    colleagues.`,
-    Heading2: `Rahil Foundation's Approach:`,
-    para5: `Rahil Foundation is Committed to Permit Individuals with the
-    Knowledge and Skills to Recognize and Thwart Phishing Attempts.
-    Our comprehensive Approach includes:
+    Heading2: `Know How it Happened`,
+    para4: `
+Harshita Kejriwal (Daughter of Arvind Kejriwal) sold her sofa in OLX. A
+Few days after No one took Interest in her sofa. Initially, Harshita was
+asked to make some payment to gain trust and then later the
+Scammer asked her to scan the QR Code. Harshita did the QR Code
+Scan immediately for 1 min After that the Money was Withdrawn
+from his Account. When Harshita Complained about this, the
+Scammer sent him the 2nd QR Code and then Rs. 14,000 were
+withdrawn from Harshita Kejriwal's Account. When Harshita came to
+know that a Scam had happened with her, she complained to the
+Cyber Cell.`,
+
+
+    Heading3: `What is OLX (Online Exchange)`,
+    content1: `OLX Market is an Online Platform where users can Buy and Sell a wide
+Variety of New or Used Goods. Sellers Create listings for Items they
+want to Sell, Providing Details and Images, while Buyers browse
+through the listings to find Products of Interest. It Acts as a Virtual
+Marketplace, Connecting Buyers and Sellers for Transactions. OLX
+Market covers a diverse range of Categories, including Electronics,
+furniture, fashion, and more. Users can communicate with each other
+through the platform to Negotiate prices, arrange meetings, and
+finalize Transactions. It offers a Convenient and Accessible way for
+people to trade goods locally, making it a popular choice for those
+seeking to declutter, find Unique items or save Money by Purchasing
+Second-Hand goods.`,
+    Heading4: `Understanding OLX Frauds:`,
+    para5: `Beware of OLX Fraud with these Simple Tips! Scammers often use
+Fake listings to Deceive Buyers. Verify the Seller's Identity by Checking
+their Ratings and Reviews. Avoid Advanced Payments or Sharing
+Personal information. Meet in Public places for Transactions and
+Inspect the item thoroughly. Trust your Instincts—If a Deal seems too
+Good to be True, it Probably is! Report Suspicious Activity to OLX and
+Local Authorities to Protect yourself and Others from Falling Victim to
+Fraud. Stay Alert, Stay Safe!.
     `,
-    content: `In an increasingly interconnected world, where our personal and professional lives revolve around digital devices, the importance of safeguarding our data cannot be overstated. Rahil Foundation is committed to promoting digital security literacy, and in this comprehensive guide, we will explore essential practices to keep your data secure on both computers and mobile devices. If you are interested in how to keep your data secure, kindly read our page Understanding the Importance of Data Security.`,
-    imgurl: "https://cdn.ttgtmedia.com/rms/onlineimages/what_is_a_blog_used_for-f_mobile.png",
-    videoUrl: "https://www.youtube.com/embed/your_video_id",
-    relatedBlogs: [1, 3],
+    para6: ``,
+    Heading5: `How to Be Safe from OLX Users Scam: `,
+    list1: `1. Verify the Seller: `,
+    content2: `Always check the Seller's profile, Ratings and Reviews on OLX. If they
+have Positive Feedback from Previous Transactions, it's a Good Sign.
+Be Cautious if the Seller is New or has Negative Reviews.`,
+    list2: `2. Avoid Advance Payments: `,
+      content3: `Always check the Seller's profile, Ratings and Reviews on OLX. If they
+have Positive Feedback from Previous Transactions, it's a Good Sign.
+Be Cautious if the Seller is New or has Negative Reviews.`,
+    list3: `3. Meet in Public Places:  `,
+      content4: `Choose safe, public locations for transactions. Avoid secluded areas,
+and if possible, bring a friend along. Meeting in a busy, well-lit spot
+reduces the risk of potential scams.`,
+
+    list4: `4. Inspect the Item Thoroughly:`,
+      content5: `Before finalizing the deal, carefully inspect the item. Ensure it
+matches the description in the listing, and there are no hidden issues.
+Don't hesitate to ask Questions about the Product's Condition.
+`,
+    list5: `5. Trust Your Instincts:`,
+      content6: `If something feels off or too good to be true, trust your instincts. Walk
+away from deals that make you uncomfortable or seem suspicious.
+Report any unusual activity to OLX for further Investigation.
+`,
+    imgurl: "https://i.pinimg.com/736x/be/10/5a/be105a358565edd621a91328f2860a36.jpg",
+
+    relatedBlogs: [ 1, 3 ],
   }
 
 ];
